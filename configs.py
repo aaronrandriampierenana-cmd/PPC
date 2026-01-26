044f0a121a975308a076c0ca494d57fa3d169aea
aaronrandriampierenana-cmd <aaron.randriampierenana@gmail.com>
2026-01-26 18:05:20 +0100

    Modify thresholds and add initial energy values
    Updated reproduction thresholds and energy values for prey and predator. Added initial energy values for prey and predator.

--- configs.py
+++ configs.py
@@ -6,13 +6,16 @@ index_pred=1
 index_herbe=2
 
 seuil_faim=40
-seuil_reproduction_proie=60
-seuil_reproduction_predateur=60
+seuil_reproduction_proie=55
+seuil_reproduction_predateur=65
 energie_max=100
-gain_repas=20
+gain_repas=25
 cout_vie=1
 facteur_reproduction=2
 qte_herbe_mangee=4
+
+energie_depart_proie=45
+energie_depart_predateur=50
 
 nouvelle_proie=b"NOUVELLE_PROIE"
 nouveau_predateur=b"NOUVEAU_PREDATEUR"
